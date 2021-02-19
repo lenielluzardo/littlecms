85ac281e5ede3e913acd87100d51578119d7fb4b
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2021-02-19 15:31:05 -0300

    [Refactor] Less Registration Form

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -11,108 +11,134 @@
     @endif
 
     <div class="form-container">
-      <form class="ll_form" action="{{ route('register')}}" method="post">
+      <form class="form registration" action="{{ route('register')}}" method="post">
 
-        <label class="f_lbl" for="name">Name</label>
-        <input class="f_field" id="name" name="name" type="text" value="{{old('name')}}">
+        <div class="form-section">
 
-        @error('name')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+            <div class="form-group">
+                <label class="form-label" for="name">Name</label>
+                <input class="form-input" id="name" name="name" type="text" value="{{old('name')}}">
 
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-        <label class="f_lbl" for="about1">Tell About Yourself</label>
-        <textarea class="comments" id="about1" name="about1" placeholder="Tell something about yourself..." value="{{old('about1')}}"></textarea>
-              @error('about1')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+            <div class="form-group">
+                <label class="form-label" for="about1">Tell About Yourself</label>
+                <textarea class="comments" id="about1" name="about1" placeholder="Tell something about yourself..." value="{{old('about1')}}"></textarea>
+                      @error('about1')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-        <label class="f_lbl" for="about2">A Little Bit More</label>
-        <textarea class="comments" id="about2" name="about2" placeholder="Tell something about yourself..." value="{{old('about2')}}"></textarea>
-              @error('about2')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+            <div class="form-group">
+                <label class="form-label" for="about2">A Little Bit More</label>
+                <textarea class="comments" id="about2" name="about2" placeholder="Tell something about yourself..." value="{{old('about2')}}"></textarea>
+                      @error('about2')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-        <label class="f_lbl" for="url1">Profile Image Url</label>
-        <input class="f_field" id="url1" name="url1" type="text" value="{{old('url1')}}">
+            <div class="form-group">
+                <label class="form-label" for="url1">Profile Image Url</label>
+                <input class="form-input" id="url1" name="url1" type="text" value="{{old('url1')}}">
 
-        @error('url1')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+                @error('url1')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-        <label class="f_lbl" for="url2">Secondary Image Url</label>
-        <input class="f_field" id="url2" name="url2" type="text" value="{{old('url2')}}">
+            <div class="form-group">
+                <label class="form-label" for="url2">Secondary Image Url</label>
+                <input class="form-input" id="url2" name="url2" type="text" value="{{old('url2')}}">
 
-        @error('url2')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+                @error('url2')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-
-        <label class="f_lbl" for="nickname">Choose A Cool Nickname</label>
-        <input class="f_field" id="nickname" name="nickname" type="text" value="{{old('nickname')}}">
-
-        @error('nickname')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-
-        <label class="f_lbl" for="email">Email</label>
-        <input class="f_field" id="email" name="email" type="text" value="{{old('email')}}">
-
-        @error('email')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-
-        <label class="f_lbl" for="recovery">Recovery Email</label>
-        <input class="f_field" id="recovery" name="recovery" type="text" value="{{old('recovery')}}">
-
-        @error('recovery')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-
-        <label class="f_lbl" for="password">Password</label>
-        <input class="f_field" id="password" name="password" type="password" >
-
-        @error('password')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-         @enderror
-
-        <label class="f_lbl" for="confirmed">Confirm Password</label>
-        <input class="f_field" id="password_confirm" name="password_confirmation" type="password" >
-
-        @error('confirmed')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-         @enderror
-
-        <input id="not_a_robot" class="f_chk" type="checkbox" name="is_human" value="on">
-        <label for="not_a_robot" > I'm not a robot</label> <br>
-
-        <div class="submit-container">
-            {{ csrf_field() }}
-          <input type="submit" name="f_button" id="f_button" class="button" value="Login">
         </div>
+        <div class="form-section">
 
+            <div class="form-group">
+                <label class="form-label" for="nickname">Choose A Cool Nickname</label>
+                <input class="form-input" id="nickname" name="nickname" type="text" value="{{old('nickname')}}">
+
+                @error('nickname')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-input" id="email" name="email" type="text" value="{{old('email')}}">
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="recovery">Recovery Email</label>
+                <input class="form-input" id="recovery" name="recovery" type="text" value="{{old('recovery')}}">
+
+                @error('recovery')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+
+                <label class="form-label" for="password">Password</label>
+                <input class="form-input" id="password" name="password" type="password" >
+
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="confirmed">Confirm Password</label>
+                <input class="form-input" id="password_confirm" name="password_confirmation" type="password" >
+
+                @error('confirmed')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+             <div class="form-group">
+                {!! NoCaptcha::display()!!}
+            </div>
+
+            <div class="button-container">
+                {{ csrf_field() }}
+              <input type="submit" name="f_button" id="f_button" class="button" value="Register">
+            </div>
+
+        </div>
       </form>
     </div>
 
   </section>
-    @endsection
+@endsection
