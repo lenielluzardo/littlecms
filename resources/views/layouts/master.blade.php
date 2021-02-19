0df03834348c133852da0e58f4273980dc3da520
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2021-02-19 10:00:32 -0300

    [Refactor] Less Main Module

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,7 @@
 <main id="main-container">
 
 
-    <div id="back-button-container">
+    <div id="web-back-icon-container">
         <a href="{{URL::previous()}}">
             <img id="web-back-icon" src="{{URL::to('./assets/icons/web-back-icon.svg')}}" alt="go-back">
         </a>
