660158dc1a2917310b65ddea751e19ea1a6d53dc
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2021-04-15 09:07:53 -0300

    [Refactor] All components Media queries

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -2,13 +2,7 @@
 
 @section('content')
 
-<section class="publication-section-container">
-    <section class="content-section-container">
-
         @include('partial.errors')
         @include('contact.form')
-
-    </section>
-</section>
 
 @endsection
--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -2,6 +2,5 @@
     <section id="sidebar-ads-section-container">
         <h2 id="sidebar-ads-section-title">Your ads can go here ;)</h2>
         <div></div>
-        <div></div>
     </section>
 </aside>
