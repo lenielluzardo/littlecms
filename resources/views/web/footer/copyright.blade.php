9709b7eb5a920f297e60b3b10257872107530ca4
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2021-08-16 23:58:25 -0300

    [RFT] Copyright

--- resources/views/web/footer/copyright.blade.php
+++ resources/views/web/footer/copyright.blade.php
@@ -1,3 +1,3 @@
 <section class="copyright">
-      <p><a href="{{ route('login') }}"> Leniel Luzardo</a>&copy 2020  | All rights reserved<p>
+      <p><a href="{{ route('login') }}"> {{ config('app.name') }}</a> &copy 2020  | All rights reserved<p>
 </section>
